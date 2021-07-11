85eaf3d30d23f8fff7d1bbfeae470c95def9ce9c
J N Sudhir <40768697+jnsudhir@users.noreply.github.com>
2021-07-11 18:54:15 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,13 @@
+class test
+{
+	public static void main(String args[])
+	{
+		System.out.println("Before call");
+		fun();
+		System.out.println("After call");
+	}
+	public static void fun()
+	{
+		System.out.println("Inside fun()");
+	}
+}
