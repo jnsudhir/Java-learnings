5ff51666e8421627a3e34900b480e6173b7d9e65
J N Sudhir <40768697+jnsudhir@users.noreply.github.com>
2021-07-11 19:23:02 +0530

    Create Passing_Parameters.java

---
+++ Passing_Parameters.java
@@ -0,0 +1,16 @@
+class test
+{
+	public static void main(String[] args)
+	{
+		int x = 10, y=5;
+		System.out.print(getmax(x, y));
+	}
+	public static int getmax(int x, int y)
+	{
+		if(x>y)
+			return x;
+		else 
+			return y;
+	}
+}
+	
